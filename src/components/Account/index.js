cf1746b8d4c47ef45c5dcc1e5af97f9d94cbcbe9
Roman Alenskiy <kuperman88@gmail.com>
2019-02-12 15:57:29 +0300

    Finished Password management

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import PasswordChangeForm from '../PasswordChange'
+import { PasswordForgetForm } from '../PasswordForget'
+
 const Account = () => (
   <div>
     <h1>Account</h1>
+    <PasswordChangeForm />
+    <PasswordForgetForm />
   </div>
 )
 
--- src/components/PasswordChange/index.js
+++ src/components/PasswordChange/index.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const PasswordChange = () => (
-  <div>
-    <h1>PasswordChange</h1>
-  </div>
-)
+import { withFirebase } from '../Firebase'
 
-export default PasswordChange
+const INITIAL_STATE = {
+  passwordOne: '',
+  passwordTwo: '',
+  error: null,
+}
+
+class PasswordChangeForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { ...INITIAL_STATE }
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+
+    const { passwordOne } = this.state
+    const { firebase } = this.props
+
+    firebase
+      .doPasswordUpdate(passwordOne)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE })
+      })
+      .catch((error) => {
+        this.setState({ error })
+      })
+  }
+
+  onChange = (event) => {
+    const { name, value } = event.target
+    this.setState({ [name]: value })
+  }
+
+  render() {
+    const { passwordOne, passwordTwo, error } = this.state
+    const isInvalid = passwordOne !== passwordTwo || passwordOne === ''
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          type="password"
+          name="passwordOne"
+          value={passwordOne}
+          onChange={this.onChange}
+          placeholder="New Password"
+        />
+
+        <input
+          type="password"
+          name="passwordTwo"
+          value={passwordTwo}
+          onChange={this.onChange}
+          placeholder="Confirm Password"
+        />
+
+        <button disabled={isInvalid} type="submit">Change My Password</button>
+
+        {error && <p>{error.message}</p>}
+
+      </form>
+    )
+  }
+}
+
+export default withFirebase(PasswordChangeForm)
